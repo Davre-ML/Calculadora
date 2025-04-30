2cc3b845f9ee9329c65c5c01cba3f50f50dc2f40
Davre-ML <davidmlopez1306@gmail.com>
2025-04-30 10:56:37 -0600

    Create sumar.py

---
+++ sumar.py
@@ -0,0 +1,10 @@
+def suma():
+  print("Has elegido suma")
+  print("Introduce 1 número")
+  num1 = int(input())
+  print("Introduce 1 número")
+  num2 = int(input())
+  suma = num1 + num2
+  print("El resultado de la suma es: ")
+  print(suma)
+suma()
