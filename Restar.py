f8524526d4d77868c205d2a50554b7ca8f84fbeb
Davre-ML <davidmlopez1306@gmail.com>
2025-04-30 11:00:55 -0600

    Create Restar.py

---
+++ Restar.py
@@ -0,0 +1,10 @@
+def resta():
+  print("Has elegido resta")
+  print("Introduce 1 número")
+  num1 = int(input())
+  print("Introduce 1 número")
+  num2 = int(input())
+  resta = num1 - num2
+  print("El resultado de la resta es: ")
+  print(resta)
+resta()
