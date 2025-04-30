e86590d0e27010a0eb4249d1b502ba565df9c8dc
Davre-ML <davidmlopez1306@gmail.com>
2025-04-30 11:11:22 -0600

    Create dividir.py

---
+++ dividir.py
@@ -0,0 +1,10 @@
+def dividir():
+  print("Has elegido división")
+  print("Introduce 1 número")
+  num1 = int(input())
+  print("Introduce 1 número")
+  num2 = int(input())
+  resultado = num1 / num2
+  print("El resultado de la división es: ")
+  print(resultado)
+dividir()
