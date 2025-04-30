3f2007af86b3f62479f0769756371cefa0e742c9
Davre-ML <davidmlopez1306@gmail.com>
2025-04-30 10:43:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 #Aquí se ejecutará el menu de opciones de la calculadora
+print("------------------------------------------")
+print("..................MENU....................")
+print("1.- SUMAR, solo 2 números")
+print("2.- RESTAR, solo 2 números")
+print("3.- MULTIPLICACIÓN, solo 2 números")
+print("4.- DIVIDIR, solo 2 números")
+print("5.- SUMA AVANZADA, sumar una cantidad N de números")
+print("Escribe los números según la opción deseada")
+operacion = int(input(""))
+print("------------------------------------------")
+
+#SUMA == 1
+#RESTA == 2
+#MULTI == 3
+#DIVIDIR == 4
+#SUM AVANZADA == 5
