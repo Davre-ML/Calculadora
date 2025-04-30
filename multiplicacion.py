dae9d833a401b4926d134ad9ff09ae4a1fa7df43
Davre-ML <davidmlopez1306@gmail.com>
2025-04-30 11:08:46 -0600

    Create multiplicacion.py

---
+++ multiplicacion.py
@@ -0,0 +1,10 @@
+def multiplicacion():
+  print("Has elegido multiplicacion")
+  print("Introduce 1 número")
+  num1 = int(input())
+  print("Introduce 1 número")
+  num2 = int(input())
+  resultado = num1 * num2
+  print("El resultado de la multiplicación es: ")
+  print(resultado)
+multiplicacion()
